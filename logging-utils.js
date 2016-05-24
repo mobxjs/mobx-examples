4b4ebc9e2f83081f1a0d8a4e4dd3ca74864c64dc
Matt Ruby <mattruby@gmail.com>
2016-05-24 13:45:33 -0500

    Trying to fix up the fiddle integration

--- logging-utils.js
+++ logging-utils.js
@@ -5,18 +5,38 @@ var logger = mobx.observable({
 	}
 });
 
+var logPrinter = mobxReact.observer(React.createClass({
+	displayName: 'printer.log',
+	render: function () {
+		return React.createElement('div', {},
+			_.map(this.props.logger.logList, function (logItem, i) {
+				return React.createElement(logItemRenderer, {logItem: logItem, key: i});
+			})
+		);
+	}
+}));
+
+var logItemRenderer = mobxReact.observer(function logItem (props) {
+	var logItem = props.logItem;
+	var children = logItem;
+
+	if(_.isObject(logItem)) {
+		children = JSON.stringify(logItem);
+	} else if(_.isArray(logItem)) {
+		children = logItem.join(',');
+	}
+
+	return React.createElement('div', null, children);
+
+});
+
 var renderLog = function () {
-	React.render(
+	ReactDOM.render(
 		React.createElement(logPrinter, {logger: logger}),
 		document.getElementById('react-log')
 	);
 };
 
-var logPrinter = mobxReact.observer(React.createClass({
-	displayName: 'printer.log',
-	render: function () {
-		return _.map(this.props.logger.logList, function (logItem) {
-			React.createElement('div', null, logItem);
-		});
-	}
-}));
+window.onload = function () {
+	renderLog();
+};
