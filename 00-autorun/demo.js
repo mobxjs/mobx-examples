9ccccf04ed6cefa3ebed02d7ff5a573f9b8c01c4
Matt Ruby <mattruby@gmail.com>
2016-05-24 11:12:40 -0500

    Simplified the example

--- 00-autorun/demo.js
+++ 00-autorun/demo.js
@@ -10,6 +10,12 @@ mobx.autorun(function () {
 	console.log(person.firstName + ' ' + person.age);
 });
 
+// this will print Matt NN 10 times
 _.times(10, function () {
 	person.age = _.random(40);
+});
+
+// this will print nothing
+_.times(10, function () {
+	person.lastName = _.random(40);
 });
