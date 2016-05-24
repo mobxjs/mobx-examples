9042a147529bf8aeaca85f0996144a383b7a844c
Matt Ruby <mattruby@gmail.com>
2016-05-24 11:09:03 -0500

    Simplified the example

--- 00-autorun/demo.js
+++ 00-autorun/demo.js
@@ -3,28 +3,13 @@ console.clear();
 var person = mobx.observable({
 	firstName: 'Matt',
 	lastName: 'Ruby',
-	age: 25,
-	fullName: function () {
-		console.count('fullName called');
-		pausecomp(500);
-		return this.firstName + ' ' + this.lastName;
-	}
+	age: 25
 });
 
 mobx.autorun(function () {
-	console.time('Autorun');
-	console.log(person.fullName + ' ' + person.age);
-	console.timeEnd('Autorun');
+	console.log(person.firstName + ' ' + person.age);
 });
 
 _.times(10, function () {
 	person.age = _.random(40);
 });
-
-// don't ever do this!!
-// noprotect
-function pausecomp (ms) {
-	ms += new Date().getTime();
-	while (new Date() < ms) {
-	}
-}
