dfe5141279c6ca3b14848f2fd63a034d5af0e17e
Matt Ruby <mattruby@gmail.com>
2016-05-24 13:10:39 -0500

    Trying to fix up the fiddle integration

---
+++ logging-utils.js
@@ -0,0 +1,22 @@
+var logger = mobx.observable({
+	logList: [],
+	log: function (message) {
+		logList.push(message);
+	}
+});
+
+var renderLog = function () {
+	React.render(
+		React.createElement(logPrinter, {logger: logger}),
+		document.getElementById('react-log')
+	);
+};
+
+var logPrinter = React.createClass({
+	displayName: 'printer.log',
+	render: function () {
+		return _.map(this.props.logger.logList, function (logItem) {
+			React.createElement('div', null, logItem);
+		});
+	}
+});
