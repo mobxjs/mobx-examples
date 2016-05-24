87cf3024242ed5023d00bbe509d1bacc4fcce8d4
Matt Ruby <mattruby@gmail.com>
2016-05-24 16:37:52 -0500

    Added extendObservable

---
+++ 30-extendObservable/demo.js
@@ -0,0 +1,30 @@
+console.clear();
+
+var Person = function (firstName, lastName, age) {
+	mobx.extendObservable(this, {
+		firstName: firstName,
+		lastName: lastName,
+		age: age,
+		fullName: function () {
+			console.count('fullName');
+			return this.firstName + ' ' + this.lastName;
+			}
+	});
+};
+
+var person = new Person('Matt', 'Ruby', 0);
+
+mobx.autorun(function () {
+	console.log(person.fullName + ' ' + person.age);
+});
+
+mobx.extendObservable(person, { nickname: 'Ruby'});
+
+mobx.autorun(function () {
+	console.log('Nickname: ' + person.nickname + ' ' + person.age);
+});
+
+person.firstName = 'Mike';
+person.firstName = 'Lissy';
+person.nickname = 'Red'
+
