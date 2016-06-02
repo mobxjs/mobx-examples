8f4530578eb09a40715b8f0c5762ecce20552a1f
mattruby <mattruby@gmail.com>
2016-06-02 15:30:50 -0500

    Tightened up the observable example

--- mobx-standalone/35-observable/demo.js
+++ mobx-standalone/35-observable/demo.js
@@ -1,27 +1,16 @@
-var personFactory = function (firstName, lastName, age) {
-	return mobx.observable({
-		firstName: firstName,
-		lastName: lastName,
-		age: age,
-		fullName: function () {
-			console.count('fullName');
-			return this.firstName + ' ' + this.lastName;
-			}
-	});
-};
-
-var person = personFactory('Matt', 'Ruby', 0);
+var person = mobx.observable({
+	firstName: 'Matt',
+	lastName: 'Ruby',
+	age: 34,
+	fullName: function () {
+		console.count('fullName');
+		return this.firstName + ' ' + this.lastName;
+	}
+});
 
 mobx.autorun(function () {
 	console.log(person.fullName + ' ' + person.age);
 });
 
-mobx.extendObservable(person, { nickname: 'Ruby'});
-
-mobx.autorun(function () {
-	console.log('Nickname: ' + person.nickname + ' ' + person.age);
-});
-
 person.firstName = 'Mike';
 person.firstName = 'Lissy';
-person.nickname = 'Red';
