ba4b740ead54aee9e6531944022ea3c28ee12107
mattruby <mattruby@gmail.com>
2016-06-24 14:00:12 -0500

    Provider experimental example

---
+++ react-examples/20-todo-provider/demo.js
@@ -0,0 +1,51 @@
+// Note how 'color' is referenced in observer then as regular props here
+var Button = mobxReact.observer(['color'], React.createClass({
+	render: function () {
+		return React.createElement(
+			"button",
+			{style: {background: this.props.color}},
+			this.props.children
+		);
+	}
+}));
+
+// Note how color is never referenced here
+var Message = React.createClass({
+	render: function () {
+		return React.DOM.div(null,
+			this.props.text,
+			" ",
+			React.createElement(
+				Button, null, "Delete"
+			)
+		);
+	}
+});
+
+// Note the Provider and color
+var MessageList = React.createClass({
+	childContextTypes: {
+		color: React.PropTypes.string
+	},
+	render: function () {
+
+		var children = this.props.messages.map(function (message) {
+			return React.createElement(Message,
+				{text: message.text, key: message.text});
+		});
+
+		return React.createElement(mobxReact.Provider, {color: 'green'}, React.DOM.div(null, children
+		));
+	}
+});
+
+ReactDOM.render(
+	React.createElement(MessageList, {
+		messages: [
+			{text: 'Message 1'},
+			{text: 'Message 2'},
+			{text: 'Message 3'}
+		]
+	}),
+	document.getElementById('mount')
+);
