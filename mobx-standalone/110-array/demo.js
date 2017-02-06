49672c8cb638e0ea0359f7a7c6914d8f161c8689
Matt Ruby <mattruby@gmail.com>
2017-02-06 16:03:56 -0600

    added simple array example

--- mobx-standalone/110-array/demo.js
+++ mobx-standalone/110-array/demo.js
@@ -1,21 +1,24 @@
 var person = mobx.observable({
-	stats: mobx.observable.map({})
+	names: [],
+	names2: [
+		{
+			foo: 'Matt'
+		}
+	]
 });
 
-mobx.autorun(function auto_statsToJS () {
-	console.log('autorun: ' + person.stats);
+mobx.autorun(function () {
+	console.log('will fire: ' + person.names);
 });
 
-person.stats.set('height', 6);
-person.stats.set('weight', 155);
-person.stats.set('height', 6);
-person.stats.set('height', '5 feet, 11 inches');
-person.stats.set('height', 5);
-person.stats.delete('height');
-
-mobx.runInAction(function () {
-	person.stats.set('weight', 160);
-	person.stats.set('height', 6);
-	person.stats.set('height', '5 feet, 11 inches');
-	person.stats.set('height', 5);
+// note how we're just observing the array, not the object values in the case
+mobx.autorun(function () {
+	console.log('wont fire: ' + person.names2);
 });
+
+person.names.push('Ruby');
+person.names.push('Red');
+person.names[1] = 'Orange';
+
+// this will not fire as nothing is watching foo
+person.names2[0].foo = 'test';
